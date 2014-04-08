ecf24b1f88f1281d90d6f7252a04779bd4d6e9d5
Allan BRUYERE <mifefr@gmail.com>
2014-04-08 09:43:39 +0200

    Allan Somerlinck
    The fils cached

---
+++ ClasseCinema/Film.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasseCinema
+{
+    class Film
+    {
+        #region Attributs
+        private string affiche;
+        private string nom;
+        #endregion Attributs
+
+        #region Opérations
+
+        public Film(string nom, string affiche)
+        {
+            this.nom = nom;
+            this.affiche = affiche;
+        }
+
+        public string getAffiche()
+        {
+            return this.affiche;
+        }
+
+        public string getNom()
+        {
+            return this.nom;
+        }
+
+        public void setAffiche(string affiche)
+        {
+            this.affiche = affiche;
+        }
+
+        public void setNom(string nom)
+        {
+            this.nom = nom;
+        }
+
+        #endregion Opérations
+
+
+    }
+}
